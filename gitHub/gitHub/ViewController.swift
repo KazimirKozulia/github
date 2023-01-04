cf37d9eeaee0c4446bf34e26565c0eab866e8c50
KazimirKozulia <kazkozsquad@gmail.com>
2023-01-04 17:41:18 +0300

    Навалил кринжа
    ?

---
+++ gitHub/gitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitHub
+//
+//  Created by Ghost on 4.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
